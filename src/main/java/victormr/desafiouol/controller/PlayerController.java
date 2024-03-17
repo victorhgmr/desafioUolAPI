5a29fad0fe612c44994af16e38310f0f3c17f48f
Victor <contato.victorhugos@gmail.com>
2024-03-17 16:18:34 -0300

    Post Mapping Player BD Commit

--- src/main/java/victormr/desafiouol/controller/PlayerController.java
+++ src/main/java/victormr/desafiouol/controller/PlayerController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,6 +19,7 @@ public class PlayerController {
     @Autowired
     private PlayerService service;
 
+    @PostMapping
     public ResponseEntity<Player> createPlayer(@RequestBody @Validated PlayerDTO player){
         Player newPlayer = service.createPlayer(player);
         return new ResponseEntity<>(newPlayer, HttpStatus.CREATED);
